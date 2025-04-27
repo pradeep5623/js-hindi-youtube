33160f2506f0961056ae937334ae4f1e6a943123
Pradeep verma <pradeepverm71@gmail.com>
2025-04-27 16:55:20 +0000

    dataType

---
+++ 1_base/02_dataType.js
@@ -0,0 +1,14 @@
+"use strict";// treat all js code as newer version
+alert(3+3) // we are using node js not broser
+let name="pradeep"
+let age=19
+let isloggedIn=false
+// number =>2 to power of 53
+//bigint 
+//string=> "" ' '
+//boolean => true/false
+// null=>standalone value
+//undefined =>
+    //symbol => unique
+//object
+console.log(typeof "pradeep");
