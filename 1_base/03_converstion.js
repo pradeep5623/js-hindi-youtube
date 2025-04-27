e460adc10a4f708ffa140c3a2b78c01a1460f941
Pradeep verma <pradeepverm71@gmail.com>
2025-04-27 17:22:28 +0000

    converstion

--- 1_base/03_converstion.js
+++ 1_base/03_converstion.js
@@ -1,6 +1,11 @@
-let score="44"
+let score="44 abc"
 
 console.log(typeof score);
-let valueInNumber=Nmuber(score)
-console.log(typeof vlaueInNumber);
+let valueInNumber=Number(score)
+console.log(   valueInNumber);
 
+//"33"=>33
+//"333abg"=>nan
+let isloggedIn=1
+let booleanIsloggedIn=Boolean(isloggedIn)
+console.log(booleanIsloggedIn);
